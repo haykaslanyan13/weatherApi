04c9cac25da9795b951066a95bb3675a032d43b5
haykaslanyan13 <85225809+haykaslanyan13@users.noreply.github.com>
2021-10-18 13:05:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ getData(name)
 
 function dateBuilder(d){
     let months = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
-    let days = ['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
